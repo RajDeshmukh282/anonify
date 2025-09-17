1fc20b818c3950b7cfd64d0612cc8edfd456f76b
Raj Deshmukh <rajdeshmukh282@gmail.com>
2025-09-18 01:03:09 +0530

    2.Basic Schema Created

--- src/model/User.model.ts
+++ src/model/User.model.ts
@@ -6,5 +6,29 @@ export interface Message extends Document{
 }
 
 const MessageSchema : Schema<Message> = new Schema({
-    
+    content :{type :String, required :true},
+    createdAt :{type :Date, required :true,default :Date.now}
 })
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isAcceptingMessage: boolean;
+    isverified: boolean;
+    messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: { type: String, required: [true, 'Username is required'],trim: true, unique: true },
+    email: { type: String, required: [true, 'Email is required'],trim: true, unique: true },
+    password: { type: String, required: [true, 'Password is required'],trim: true },
+    verifyCode: { type: String, required: [true, 'Verify code is required'],trim: true },
+    verifyCodeExpiry: { type: Date, required: [true, 'Verify code expiry is required'] },
+    isAcceptingMessage: { type: Boolean, required: [true, 'Accepting message status is required'] },
+    isverified: { type: Boolean, required: [true, 'Verification status is required'], default: false },
+    messages: { type: [MessageSchema]}
+})
+  
